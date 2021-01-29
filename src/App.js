f19b4a52f7f5be965f8418085cff79beb7569688
dharshan-97 <sivdharshan67@gmail.com>
2021-01-29 17:03:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          lets start react in docker
         </a>
       </header>
     </div>
